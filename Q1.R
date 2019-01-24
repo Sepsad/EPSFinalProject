cd36b97e8074b3d08557960f7b980e76ba0da76a
Sepsad <sepehrsdp@gmail.com>
2019-01-24 15:43:27 +0330

    4

--- Q1.R
+++ Q1.R
@@ -1,4 +1,17 @@
-MyData <- read.csv(file="FIFA18 - Ultimate Team players.csv", header=TRUE, sep=",")
-agnkangkajng
-abamgnalkgq
-agknagkag
+[33mcommit 0b1f9cab8a8c33a6a90108660c102f145938753c[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Sepsad <sepehrsdp@gmail.com>
+Date:   Thu Jan 24 15:40:18 2019 +0330
+
+    'salavat'
+
+[33mcommit 33cd5f3b3e386b821873f9ad323c39cfce2c9674[m
+Author: Sepsad <sepehrsdp@gmail.com>
+Date:   Thu Jan 24 14:30:35 2019 +0330
+
+    q1'
+
+[33mcommit 07949b595fc9a35b8f9d2503d7b25ed936c05324[m
+Author: Sepsad <sepehrsdp@gmail.com>
+Date:   Thu Jan 24 14:29:31 2019 +0330
+
+    'sep'
